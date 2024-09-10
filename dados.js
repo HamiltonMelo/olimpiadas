a72cc3368b6ded6e5dcc07006710675115071b70
Hame <hmiltonmelo@gmail.com>
2024-09-10 00:01:11 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal é uma skatista brasileira, conhecida como Fadinha, que se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris 2024, sendo a primeira brasileira a conquistar o ouro em sua primeira participação olímpica na categoria +78kg.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, incluindo o ouro no solo feminino nas Olimpíadas de Paris 2024, superando a rival americana Simone Biles.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginasta ouro"
+    }
+];
